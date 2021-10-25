a36f80eb8d526ec018660814d4703e8f9b20a524
mara1t <88665544+mara1t@users.noreply.github.com>
2021-10-26 01:01:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "header_stk.h"
-#define NDEBUG 1
+#include "stk_header.h"
 
 int main()
 {
     Stack stk = {};
-    StackCtor (&stk, 10);
-    StackPush (&stk, 10);
-    StackPush (&stk, 20);
-    double x = StackPop (&stk);
-    double x1 = 0;
-   // if (StackPop(&stk, &x1) == ERR_UNDERFLOW) 
-    printf("%lg", StackPop(&stk));
-    for(int i = 0; i < 100; i++)
-    {
-        StackPush(&stk, i);
-    }
+    StackCtor (&stk, BASE_STK_SIZE);
+    StackPush (&stk, 1);
+    StackPush (&stk, 3); 
     StackDtor(&stk);
     return 0;
 }
-
